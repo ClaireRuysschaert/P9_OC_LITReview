53809640dbf09c230e3b7ff35b87343d32a985e5
ClaireRuysschaert <netclaire@hotmail.fr>
2024-01-05 17:38:25 +0100

    feat: create login/logout

--- LITReview/urls.py
+++ LITReview/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", LoginView.as_view(template_name="authentication/login.html", redirect_authenticated_user=True), name='login'),
-    path("logout/", LogoutView.as_view(), name="logout")
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("home/", blog.views.home, name="home"),
 ]
--- authentication/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from . import forms
-
-def login(request):
-    form = forms.LoginForm()
-    return render(request, "registration/login.html", {"form": form})
