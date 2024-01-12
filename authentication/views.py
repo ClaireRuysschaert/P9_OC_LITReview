7966b38b6ce85e6a727595282f5153ec3ca7ade1
ClaireRuysschaert <netclaire@hotmail.fr>
2024-01-12 17:42:42 +0100

    feat: Add success message after account creation

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import LITUserCreationForm
@@ -6,3 +7,8 @@ class SignUpView(CreateView):
     form_class = LITUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "authentication/signup.html"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Votre compte a bien été créé, vous pouvez maintenant vous connecter.")
+        return response
