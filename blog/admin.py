0a3fde3c8ced57ec50c4593db987a2dd78e8a18c
ClaireRuysschaert <netclaire@hotmail.fr>
2024-01-26 14:12:28 +0100

    feat: Add Ticket and Review models with migrations

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Ticket, Review
 
-# Register your models here.
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+
+class TicketAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline]
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+from LITReview.settings import AUTH_USER_MODEL
+from PIL import Image
+
+class Ticket(models.Model):
+    """
+    User Asking's critics for a book or a litterary article
+    """
+    user = models.ForeignKey(to=AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+    def rezise_image(self):
+        image = Image.open(self.image)
+        image.thumbnail((300, 300))
+        image.save(self.image.path)
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image:
+            self.rezise_image()
+
+class Review(models.Model):
+    """
+    User's critics for a book or a litterary article
+    """
+    user = models.ForeignKey(to=AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    rating = models.PositiveSmallIntegerField(blank=True, null=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    title = models.CharField(max_length=128)
+    content = models.TextField(max_length=8192, blank=True)
+    
+    def __str__(self):
+        return self.content
