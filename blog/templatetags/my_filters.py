c186baeb69d0bc3a1845450d716c9adce30ef0ca
ClaireRuysschaert <netclaire@hotmail.fr>
2024-02-08 18:08:53 +0100

    feat: Add custom template filters to blog app

---
+++ blog/templatetags/my_filters.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def classname(obj):
+    return obj.__class__.__name__
+
+@register.filter
+def my_range(value):
+    return range(value)
+
+@register.filter
+def substraction(value, arg):
+    return value - arg
